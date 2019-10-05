1802f10aa3945ad66c3dc369e95319329507d828
eswarijayakumar <eswarijayakumar27@gmail.com>
2019-10-05 21:37:25 +0530

    Added Gatsby site template

--- src/components/layout.js
+++ src/components/layout.js
@@ -36,9 +36,9 @@ const Layout = ({ children }) => {
       >
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
+         {/*} © {new Date().getFullYear()}, Built with
           {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+      <a href="https://www.gatsbyjs.org">Gatsby</a>*/}
         </footer>
       </div>
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,12 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <h1>Hi #WomenInTech</h1>
+    <p>Welcome !!!</p>
+    {/*<div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+<Link to="/page-2/">Go to page 2</Link>*/}
   </Layout>
 )
 
